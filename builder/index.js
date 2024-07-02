087905894e989f00629e52f6cce363495c927f5c
Greg Walker <git@greg.email>
2024-07-01 20:13:54 -0500

    initial deploy
    add forecast and fire zones

--- builder/index.js
+++ builder/index.js
@@ -1,6 +1,11 @@
 const fs = require("node:fs/promises");
 const mariadb = require("mariadb");
 
+// You've gotta have the beta.weather.gov database running locally in order for
+// this to work. It's the one documented via Docker configs at:
+//
+// http://github.com/weather-gov/weather.gov/
+
 const main = async () => {
   const db = await mariadb.createConnection({
     user: "drupal",
@@ -11,11 +16,45 @@ const main = async () => {
     ssl: { rejectUnauthorized: false },
   });
 
-  const wfos = await db.query(
+  const wfoData = await db.query(
     "SELECT wfo,ST_ASGEOJSON(shape) as shape FROM weathergov_geo_cwas",
   );
+
+  const forecastZoneData = await db.query(
+    "SELECT id,ST_ASGEOJSON(shape) AS shape FROM weathergov_geo_zones WHERE id LIKE '%/zones/forecast/%'",
+  );
+
+  const fireZoneData = await db.query(
+    "SELECT id,ST_ASGEOJSON(shape) AS shape FROM weathergov_geo_zones WHERE id LIKE '%/zones/fire/%'",
+  );
+
   await db.end();
 
-  await fs.writeFile("./wfos.geojson", JSON.stringify(wfos));
+  const wfos = wfoData.map(({ wfo: id, shape }) => ({
+    type: "Feature",
+    geometry: shape,
+    properties: { id },
+  }));
+
+  await fs.writeFile("./docs/wfos.geojson", JSON.stringify(wfos));
+
+  const forecastZones = forecastZoneData.map(({ id, shape }) => ({
+    type: "Feature",
+    geometry: shape,
+    properties: { id: id.split("/").pop() },
+  }));
+
+  await fs.writeFile(
+    "./docs/forecast-zones.geojson",
+    JSON.stringify(forecastZones),
+  );
+
+  const fireZones = fireZoneData.map(({ id, shape }) => ({
+    type: "Feature",
+    geometry: shape,
+    properties: { id: id.split("/").pop() },
+  }));
+
+  await fs.writeFile("./docs/fire-zones.geojson", JSON.stringify(fireZones));
 };
 main();
