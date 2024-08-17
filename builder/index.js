814534ccdfe58b7954fcbc126a989d689952c64d
Greg Walker <git@greg.email>
2024-08-17 13:37:15 -0500

    wrap longitude so everything is negative

--- builder/index.js
+++ builder/index.js
@@ -16,44 +16,71 @@ const main = async () => {
     ssl: { rejectUnauthorized: false },
   });
 
-  const wfoData = await db.query(
-    "SELECT wfo,ST_ASGEOJSON(shape) as shape FROM weathergov_geo_cwas",
-  );
-
-  const forecastZoneData = await db.query(
-    "SELECT id,ST_ASGEOJSON(shape) AS shape FROM weathergov_geo_zones WHERE id LIKE '%/zones/forecast/%'",
-  );
-
-  const fireZoneData = await db.query(
-    "SELECT id,ST_ASGEOJSON(shape) AS shape FROM weathergov_geo_zones WHERE id LIKE '%/zones/fire/%'",
-  );
+  const [wfoData, forecastZoneData, fireZoneData] = await Promise.all([
+    db.query(
+      "SELECT wfo,ST_ASGEOJSON(shape) as shape FROM weathergov_geo_cwas",
+    ),
+    await db.query(
+      "SELECT id,ST_ASGEOJSON(shape) AS shape FROM weathergov_geo_zones WHERE id LIKE '%/zones/forecast/%'",
+    ),
+    await db.query(
+      "SELECT id,ST_ASGEOJSON(shape) AS shape FROM weathergov_geo_zones WHERE id LIKE '%/zones/fire/%'",
+    ),
+  ]);
 
   await db.end();
 
-  const wfos = wfoData.map(({ wfo: id, shape }) => ({
-    type: "Feature",
-    geometry: shape,
-    properties: { id },
-  }));
+  const wrapLongitude = (shape) => {
+    if (
+      Array.isArray(shape) &&
+      shape.length === 2 &&
+      !Number.isNaN(+shape[0]) &&
+      !Number.isNaN(+shape[1])
+    ) {
+      if (shape[0] > 0) {
+        shape[0] = shape[0] - 360;
+      }
+    } else if (Array.isArray(shape)) {
+      shape.forEach(wrapLongitude);
+    } else if (shape.coordinates) {
+      wrapLongitude(shape.coordinates);
+    }
+  };
+
+  const wfos = wfoData.map(({ wfo: id, shape }) => {
+    wrapLongitude(shape);
+
+    return {
+      type: "Feature",
+      geometry: shape,
+      properties: { id },
+    };
+  });
 
   await fs.writeFile("./docs/wfos.geojson", JSON.stringify(wfos));
 
-  const forecastZones = forecastZoneData.map(({ id, shape }) => ({
-    type: "Feature",
-    geometry: shape,
-    properties: { id: id.split("/").pop() },
-  }));
+  const forecastZones = forecastZoneData.map(({ id, shape }) => {
+    wrapLongitude(shape);
+    return {
+      type: "Feature",
+      geometry: shape,
+      properties: { id: id.split("/").pop() },
+    };
+  });
 
   await fs.writeFile(
     "./docs/forecast-zones.geojson",
     JSON.stringify(forecastZones),
   );
 
-  const fireZones = fireZoneData.map(({ id, shape }) => ({
-    type: "Feature",
-    geometry: shape,
-    properties: { id: id.split("/").pop() },
-  }));
+  const fireZones = fireZoneData.map(({ id, shape }) => {
+    wrapLongitude(shape);
+    return {
+      type: "Feature",
+      geometry: shape,
+      properties: { id: id.split("/").pop() },
+    };
+  });
 
   await fs.writeFile("./docs/fire-zones.geojson", JSON.stringify(fireZones));
 };
