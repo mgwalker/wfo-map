5d0e217df86ec7462344d0dd8e291f12983548a1
Greg Walker <greg.walker@adhocteam.us>
2025-09-25 12:17:21 -0500

    oopty

--- builder/index.js
+++ builder/index.js
@@ -1,5 +1,5 @@
-const fs = require("node:fs/promises");
-const mariadb = require("mariadb");
+import fs from "node:fs/promises";
+import { Client } from "pg";
 
 // You've gotta have the beta.weather.gov database running locally in order for
 // this to work. It's the one documented via Docker configs at:
@@ -7,24 +7,43 @@ const mariadb = require("mariadb");
 // http://github.com/weather-gov/weather.gov/
 
 const main = async () => {
-  const db = await mariadb.createConnection({
+  const db = new Client({
     user: "drupal",
     password: "drupal",
     database: "weathergov",
     host: "localhost",
-    port: 3306,
-    ssl: { rejectUnauthorized: false },
   });
+  await db.connect();
 
-  const [wfoData, forecastZoneData, fireZoneData] = await Promise.all([
+  const [
+    wfoData,
+    forecastZoneData,
+    fireZoneData,
+    marineCoastalData,
+    marineOffshoreData,
+    countyData,
+    stateData,
+  ] = await Promise.all([
     db.query(
       "SELECT wfo,ST_ASGEOJSON(shape) as shape FROM weathergov_geo_cwas",
     ),
     await db.query(
-      "SELECT id,ST_ASGEOJSON(shape) AS shape FROM weathergov_geo_zones WHERE id LIKE '%/zones/forecast/%'",
+      "SELECT id,ST_ASGEOJSON(shape) AS shape FROM weathergov_geo_zones WHERE type='forecast'",
     ),
     await db.query(
-      "SELECT id,ST_ASGEOJSON(shape) AS shape FROM weathergov_geo_zones WHERE id LIKE '%/zones/fire/%'",
+      "SELECT id,ST_ASGEOJSON(shape) AS shape FROM weathergov_geo_zones WHERE type='fire'",
+    ),
+    await db.query(
+      "SELECT id,ST_ASGEOJSON(shape) AS shape FROM weathergov_geo_zones WHERE type='marine:coastal'",
+    ),
+    await db.query(
+      "SELECT id,ST_ASGEOJSON(shape) AS shape FROM weathergov_geo_zones WHERE type='marine:offshore'",
+    ),
+    await db.query(
+      "SELECT countyname,st,ST_ASGEOJSON(shape) AS shape FROM weathergov_geo_counties",
+    ),
+    await db.query(
+      "SELECT name,ST_ASGEOJSON(shape) AS shape FROM weathergov_geo_states",
     ),
   ]);
 
@@ -47,41 +66,61 @@ const main = async () => {
     }
   };
 
-  const wfos = wfoData.map(({ wfo: id, shape }) => {
-    wrapLongitude(shape);
+  const wfos = wfoData.rows.map(({ wfo: id, shape }) => {
+    const geojson = JSON.parse(shape);
+    wrapLongitude(geojson);
 
     return {
       type: "Feature",
-      geometry: shape,
+      geometry: geojson,
       properties: { id },
     };
   });
 
   await fs.writeFile("./docs/wfos.geojson", JSON.stringify(wfos));
 
-  const forecastZones = forecastZoneData.map(({ id, shape }) => {
-    wrapLongitude(shape);
+  const doZones = async (dbZones, file) => {
+    const zones = dbZones.rows.map(({ id, shape }) => {
+      const geojson = JSON.parse(shape);
+      wrapLongitude(geojson);
+
+      return {
+        type: "Feature",
+        geometry: geojson,
+        properties: { id: id.split("/").pop() },
+      };
+    });
+
+    await fs.writeFile(file, JSON.stringify(zones));
+  };
+
+  await doZones(forecastZoneData, "./docs/forecast-zones.geojson");
+  await doZones(fireZoneData, "./docs/fire-zones.geojson");
+  await doZones(marineCoastalData, "./docs/marine-coastal-zones.geojson");
+  await doZones(marineOffshoreData, "./docs/marine-offshore-zones.geojson");
+
+  const counties = countyData.rows.map(({ countyname, st, shape }) => {
+    const geojson = JSON.parse(shape);
+    wrapLongitude(geojson);
+
     return {
       type: "Feature",
-      geometry: shape,
-      properties: { id: id.split("/").pop() },
+      geometry: geojson,
+      properties: { id: `${countyname}, ${st}`, name: countyname, state: st },
     };
   });
+  await fs.writeFile("./docs/counties.geojson", JSON.stringify(counties));
 
-  await fs.writeFile(
-    "./docs/forecast-zones.geojson",
-    JSON.stringify(forecastZones),
-  );
+  const states = stateData.rows.map(({ name, shape }) => {
+    const geojson = JSON.parse(shape);
+    wrapLongitude(geojson);
 
-  const fireZones = fireZoneData.map(({ id, shape }) => {
-    wrapLongitude(shape);
     return {
       type: "Feature",
-      geometry: shape,
-      properties: { id: id.split("/").pop() },
+      geometry: geojson,
+      properties: { id: name },
     };
   });
-
-  await fs.writeFile("./docs/fire-zones.geojson", JSON.stringify(fireZones));
+  await fs.writeFile("./docs/states.geojson", JSON.stringify(states));
 };
 main();
